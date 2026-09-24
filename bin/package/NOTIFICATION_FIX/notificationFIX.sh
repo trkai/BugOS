345918b3f1aeac52f970fc9d6bfc4bba62ae7843
trkai <161140493+trkai@users.noreply.github.com>
2026-09-24 23:52:01 +0700

    Update notificationFIX.sh

--- bin/package/NOTIFICATION_FIX/notificationFIX.sh
+++ bin/package/NOTIFICATION_FIX/notificationFIX.sh
@@ -6,15 +6,15 @@ regionTYPE=$(cat $WORK_DIR/bin/ddevice/device_type.txt)
 if [[ $regionTYPE == *"Global"* ]]; then
 warn "No Support Global ROM!Skipping..."
 else
-patch "Start Notification FIX..."
-if [[ $AndroidVER == "13" ]];then
+
+if [[ $AndroidVER == "12" ]];then
+    bash $WORK_DIR/bin/package/NOTIFICATION_FIX/A12/RUN.SH
+elif [[ $AndroidVER == "13" ]];then
     bash $WORK_DIR/bin/package/NOTIFICATION_FIX/A13/RUN.SH
 elif [[ $AndroidVER == "14" ]];then
     bash $WORK_DIR/bin/package/NOTIFICATION_FIX/A14/RUN.SH
 elif [[ $AndroidVER == "15" ]];then
     bash $WORK_DIR/bin/package/NOTIFICATION_FIX/A15/RUN.SH
-elif [[ $AndroidVER == "16" ]];then
-    bash $WORK_DIR/bin/package/NOTIFICATION_FIX/A16/RUN.SH
 fi
 
 fi

f5eafc0af06ea46c9a640d49d993d2291082ebc9
trkai <161140493+trkai@users.noreply.github.com>
2026-09-25 13:16:31 +0700

    Update patchpackage.sh

--- bin/package/patchpackage.sh
+++ bin/package/patchpackage.sh
@@ -4,7 +4,7 @@ source $work_dir/functions.sh
 mods "Add Package..."
 target_dir="$work_dir/bin/package/"
 
-bash $target_dir/COREPATCH/update.sh
+# bash $target_dir/COREPATCH/update.sh
 bash $target_dir/DISABLE_AVB/DISABLEavb.sh
 bash $target_dir/KouseiPatcher/update.sh
 bash $target_dir/NOTIFICATION_FIX/notificationFIX.sh
